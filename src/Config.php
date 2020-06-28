392875f383bd9ddf69750ab933af4a2a4cb91ea2
Strannik4884 <strannik4884@mail.ru>
2020-06-28 19:08:31 +0300

    Fix config loader

--- src/Config.php
+++ src/Config.php
@@ -1,10 +1,11 @@
 <?php
-require_once ('ConfigException.php');
+require_once('Exceptions/ConfigException.php');
 
 class Config
 {
     private const configFile = "../src/config.ini";
     private $key;
+    private $domain;
 
     // config class construct
     function __construct() {
@@ -15,19 +16,25 @@ class Config
         // read config file
         $ini_array = parse_ini_file(self::configFile, true);
         // check mandatory variables in config file
-        if (!isset($ini_array['API']['key'])) {
+        if (!isset($ini_array['API']['key']) and !isset($ini_array['API']['domain'])) {
             throw (new ConfigException("Config file corrupted"));
         }
         // check empty config variables
-        if (empty($ini_array['API']['key'])) {
+        if (empty($ini_array['API']['key']) and empty($ini_array['API']['domain'])) {
             throw (new ConfigException("You must set API config variables"));
         }
         // set properties
         $this->key = $ini_array['API']['key'];
+        $this->domain = $ini_array['API']['domain'];
     }
 
     public function getKey(): string
     {
         return $this->key;
+    }
+
+    public function getDomain(): string
+    {
+        return $this->domain;
     }
 }
