ef1e0349152b4d6a737c58b463559430542672a4
Strannik4884 <strannik4884@mail.ru>
2020-06-28 20:41:57 +0300

    Code refactoring

--- src/GeocoderAPI.php
+++ src/GeocoderAPI.php
@@ -13,20 +13,58 @@ class GeocoderAPI
         $this->config = new Config();
     }
 
+    // get nearest metro for geoobject by coordinates
+    private function searchMetro(string $coordinates): array
+    {
+        // query parameters
+        $queryParameters = [
+            'apikey' => $this->config->getKey(),
+            'geocode' => $coordinates,
+            'kind' => 'metro',
+            'results' => 1,
+            'format' => 'json'
+        ];
+        // read response from API
+        $response = file_get_contents($this->config->getDomain() . '?' . http_build_query($queryParameters));
+        // if error
+        if($response === false){
+            throw new APIException('Error while accessing the API service');
+        }
+        // decode response from json to array
+        $response = json_decode($response, true);
+        $metro = array();
+        $metro['count'] = (int) $response['response']['GeoObjectCollection']['metaDataProperty']['GeocoderResponseMetaData']['found'];
+        // if metro not found
+        if($metro['count'] == 0)
+        {
+            return $metro;
+        }
+        // set metro properties
+        $metro['metroName'] = $response['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['metaDataProperty']['GeocoderMetaData']['text'];
+        $metro['metroCoordinates'] = $response['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['Point']['pos'];
+        return $metro;
+    }
+
+    // search geoobject by address
     public function searchAddress(string $address): array
     {
+        // query parameters
         $queryParameters = [
             'apikey' => $this->config->getKey(),
             'geocode' => $address,
             'format' => 'json',
         ];
+        // read response from API
         $response = file_get_contents($this->config->getDomain() . '?' . http_build_query($queryParameters));
+        // if error
         if($response === false){
             throw new APIException('Error while accessing the API service');
         }
+        // decode response from json to array
         $response = json_decode($response, true);
         $geoObjects = array();
         $results = $response['response']['GeoObjectCollection']['featureMember'];
+        // create geoobjects array
         foreach ($results as $item)
         {
             $geoObject = new GeoObject();
@@ -41,30 +79,5 @@ class GeocoderAPI
             array_push($geoObjects, $geoObject);
         }
         return $geoObjects;
-    }
-
-    private function searchMetro(string $coordinates): array
-    {
-        $queryParameters = [
-            'apikey' => $this->config->getKey(),
-            'geocode' => $coordinates,
-            'kind' => 'metro',
-            'results' => 1,
-            'format' => 'json'
-        ];
-        $response = file_get_contents($this->config->getDomain() . '?' . http_build_query($queryParameters));
-        if($response === false){
-            throw new APIException('Error while accessing the API service');
-        }
-        $response = json_decode($response, true);
-        $metro = array();
-        $metro['count'] = (int) $response['response']['GeoObjectCollection']['metaDataProperty']['GeocoderResponseMetaData']['found'];
-        if($metro['count'] == 0)
-        {
-            return $metro;
-        }
-        $metro['metroName'] = $response['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['metaDataProperty']['GeocoderMetaData']['text'];
-        $metro['metroCoordinates'] = $response['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['Point']['pos'];
-        return $metro;
     }
 }
